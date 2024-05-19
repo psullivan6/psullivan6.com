6455bc2271c1a8323331701ac31f838b45838322
Patrick Sullivan <psullivan6@gmail.com>
2024-05-18 23:09:47 -0700

    Tailwind + custom font + index layout

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,24 @@
+import { LinksFunction } from '@remix-run/node';
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
-import Navigation from './components/Navigation/Navigation';
-import './root.styles.css';
+import stylesheet from '~/root.styles.css?url';
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesheet }];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-surface-var">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik+Mono+One&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <Meta />
         <Links />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
       </head>
       <body>
         {children}
@@ -22,10 +30,5 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return (
-    <>
-      <Navigation />
-      <Outlet />
-    </>
-  );
+  return <Outlet />;
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,21 +1,81 @@
 import type { MetaFunction } from '@remix-run/node';
+import Logo from '~/components/Logo';
 
 export const meta: MetaFunction = () => {
   return [
     { title: 'Patrick Sullivan' },
-    { name: 'description', content: "Welcome to my website, I'm Patrick Sullivan" },
+    {
+      name: 'description',
+      content: "Hey, I'm Patrick Sullivan and this is my website. Thanks for visiting.",
+    },
   ];
 };
 
+type Color = {
+  primary: string;
+  secondary: string;
+  surface: string;
+};
+
+function randomNoRepeats(array: Color[]) {
+  let copy = array.slice(0);
+  return function () {
+    if (copy.length < 1) {
+      copy = array.slice(0);
+    }
+    const index = Math.floor(Math.random() * copy.length);
+    const item = copy[index];
+    copy.splice(index, 1);
+    return item;
+  };
+}
+
 export default function Index() {
+  const colors = [
+    {
+      surface: '069',
+      primary: 'f90',
+      secondary: '903',
+    },
+    {
+      surface: '30c',
+      primary: 'f69',
+      secondary: '3c6',
+    },
+    {
+      surface: 'f3f3f3',
+      primary: '333',
+      secondary: '666',
+    },
+    {
+      surface: '000',
+      primary: 'fff',
+      secondary: '666',
+    },
+  ] satisfies Color[];
+  const getColor = randomNoRepeats(colors);
+
+  const handleColorToggle = () => {
+    const color = getColor();
+    document.documentElement.style.setProperty('--colors-surface', `#${color.surface}`);
+    document.documentElement.style.setProperty('--colors-primary', `#${color.primary}`);
+    document.documentElement.style.setProperty('--colors-secondary', `#${color.secondary}`);
+  };
+
   return (
-    <div>
-      <h1>Hey, welcome to psullivan6.com</h1>
-      <ul>
-        <li>
-          <a href="/playground">Playground</a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <main className="w-full h-[calc(100%-2rem)] p-24 bg-surface-var text-primary-var flex flex-col transition-colors">
+        <button
+          onClick={handleColorToggle}
+          className="absolute left-1/2 -translate-x-1/2 focus:outline-2 outline-offset-4 outline-primary-var"
+        >
+          <Logo className="box-content  p-1 stroke-primary-var stroke-4 fill-transparent hover:fill-current transition-colors overflow-visible" />
+        </button>
+        <h1 className="title w-3/4 text-clamp-huge leading-[0.8] font-black uppercase mt-auto ml-auto text-right">
+          Hey, I&rsquo;m Patrick&nbsp;&nbsp;&nbsp; Sullivan
+        </h1>
+      </main>
+      <div className="bg-secondary-var h-full">asdf</div>
+    </>
   );
 }
