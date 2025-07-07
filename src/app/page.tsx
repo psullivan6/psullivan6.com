f16551487870a81b88cf6a8d8ebb28583bc80cc2
Patrick Sullivan <psullivan6@gmail.com>
2025-07-07 16:24:27 -0700

    Next setup

---
+++ src/app/page.tsx
@@ -0,0 +1,50 @@
+const HomePage = () => {
+  return (
+    <main className="prose lg:prose-xl p-6 mx-auto my-6 prose-headings:mt-0">
+      <article className="">
+        <h1>Patrick Sullivan</h1>
+        <h2>Subheading</h2>
+        <p>
+          For years parents have espoused the health benefits of eating garlic bread with cheese to
+          their children, with the food earning such an iconic status in our culture that kids will
+          often dress up as warm, cheesy loaf for Halloween.
+        </p>
+        <p>
+          But a recent study shows that the celebrated appetizer may be linked to a series of rabies
+          cases springing up around the country.
+        </p>
+      </article>
+
+      <article>
+        <h2>Content and Things</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, animi! Laboriosam, cum
+          autem. Dicta, reprehenderit quos exercitationem, autem voluptatem voluptatibus animi
+          error, commodi ex voluptates dolor asperiores perspiciatis atque facere.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, animi! Laboriosam, cum
+          autem. Dicta, reprehenderit quos exercitationem, autem voluptatem voluptatibus animi
+          error, commodi ex voluptates dolor asperiores perspiciatis atque facere.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, animi! Laboriosam, cum
+          autem. Dicta, reprehenderit quos exercitationem, autem voluptatem voluptatibus animi
+          error, commodi ex voluptates dolor asperiores perspiciatis atque facere.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, animi! Laboriosam, cum
+          autem. Dicta, reprehenderit quos exercitationem, autem voluptatem voluptatibus animi
+          error, commodi ex voluptates dolor asperiores perspiciatis atque facere.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, animi! Laboriosam, cum
+          autem. Dicta, reprehenderit quos exercitationem, autem voluptatem voluptatibus animi
+          error, commodi ex voluptates dolor asperiores perspiciatis atque facere.
+        </p>
+      </article>
+    </main>
+  );
+};
+
+export default HomePage;
